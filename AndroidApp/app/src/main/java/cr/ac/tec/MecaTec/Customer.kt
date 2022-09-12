ff56b6b66b0fb1f246f8eb8c2c9c3164dc4f6e8c
FMonge <fmonge13@estudiantec.cr>
2022-09-11 22:32:43 -0600

    Login validation

---
+++ AndroidApp/app/src/main/java/cr/ac/tec/MecaTec/Customer.kt
@@ -0,0 +1,11 @@
+package cr.ac.tec.mecatec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class customer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer)
+    }
+}
