d407c24ac308faa51a2eae6ce3f5111dd7bde8bb
andyrrr <maxtay27@hotmail.com>
2022-09-16 20:03:49 -0600

    Interfaz: login, Citas y Reportes

--- vistaTaller/src/app/app.module.ts
+++ vistaTaller/src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CitasComponent } from './citas/citas.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ReportesComponent } from './reportes/reportes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CitasComponent,
+    ClientesComponent,
+    ReportesComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:"", component:LoginComponent},
+      {path:"login", component:LoginComponent},
+      {path:"citas", component:CitasComponent},
+      {path:"clientes", component:ClientesComponent},
+      {path:"reportes", component:ReportesComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
