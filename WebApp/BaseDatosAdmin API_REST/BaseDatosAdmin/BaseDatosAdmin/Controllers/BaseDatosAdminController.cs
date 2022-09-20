80045beb1c09db74111c26301130ac820c928d9a
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2022-09-20 15:36:20 -0600

    Update BaseDatosAdminController.cs

--- WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Controllers/BaseDatosAdminController.cs
+++ WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Controllers/BaseDatosAdminController.cs
@@ -291,6 +291,7 @@ namespace BaseDatosAdmin.Controllers
                 {
                     Cliente_Direcciones cliente_Direcciones = new Cliente_Direcciones(cdInterface.idCliente, cdInterface.provincia,
                         cdInterface.canton, cdInterface.distrito);
+                    string result = cliente_DireccionesList.addElementToJson(cliente_Direcciones);
                     var clienteDireccionesTemp = cliente_DireccionesList.list.FindAll(c => c.idCliente == cdInterface.idCliente);
                     return Ok(clienteDireccionesTemp);
                 }
@@ -333,6 +334,7 @@ namespace BaseDatosAdmin.Controllers
                || (clielteTelefonosTemp2 == null))
                 {
                     Cliente_Telefonos cliente_Telefonos = new Cliente_Telefonos(ctInterface.idCliente, ctInterface.telefono);
+                    string result = cliente_TelefonosList.addElementToJson(cliente_Telefonos
                     var clienteTelefonosTemp = cliente_TelefonosList.list.FindAll(c => c.idCliente == ctInterface.idCliente);
                     return Ok(clienteTelefonosTemp);
                 }
