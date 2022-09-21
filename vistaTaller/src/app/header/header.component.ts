f9af6ab302c1fc680bb6de75d375f1bbfb74960e
andyrrr <maxtay27@hotmail.com>
2022-09-20 23:49:15 -0600

    documentación

--- vistaTaller/src/app/header/header.component.ts
+++ vistaTaller/src/app/header/header.component.ts
@@ -10,10 +10,13 @@ import { UserService } from '../services/user.service';
 export class HeaderComponent implements OnInit {
   constructor(private route:Router, private aoth:UserService){}
 
-
+  /*Consulta a la base de datos si el usuario loggeado es un Trabajador 
+  return: boolean*/
   esTrabajador(){
     return this.aoth.isTrabajador()
   }
+  /*Consulta a la base de datos si el usuario loggeado es un Cliente 
+  return: boolean*/
   esCliente(){
     return this.aoth.isCliente()
   }
