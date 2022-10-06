1beabf0caa3a60cf4e01104a8896970580a67711
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2022-10-06 15:38:33 -0600

    Métodos multiget y get de cliente

--- WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Base de datos/Entidad.cs
+++ WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Base de datos/Entidad.cs
@@ -4,11 +4,15 @@ using Microsoft.AspNetCore.Server.IIS.Core;
 
 namespace BaseDatosAdmin.Base_de_datos
 {
+    /// <summary>
+    /// Clase abstracta y genérica para el manejo de entidades
+    /// </summary>
+    /// <typeparam name="T">Tipo de clase con la cual se desea realizar las listas de la base de datos</typeparam>
     public abstract class Entidad<T>
     {
         private string jsonFileName { get; set; }
 
-        private string path = @"C:/Users/maxta/GitHub/MecaTEC/WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Base de datos/";
+        private string path = @"C:/Users/jpabl/Documents/GitHub/MecaTEC/WebApp/BaseDatosAdmin API_REST/BaseDatosAdmin/BaseDatosAdmin/Base de datos/";
 
         private StreamReader? reader;
 
@@ -24,10 +28,13 @@ namespace BaseDatosAdmin.Base_de_datos
             this.loadJson();
 
         }
+        /// <summary>
+        /// Método encargado de obtener el json a partir de los datos guardados localmente
+        /// </summary>
+        /// <returns></returns>
         public string getJsonString()
         {
-            Console.WriteLine(this.path);
-            Console.WriteLine(this.jsonFileName);
+
 
             reader = new StreamReader(this.path + this.jsonFileName);
             string jsonString = reader.ReadToEnd();
@@ -35,12 +42,18 @@ namespace BaseDatosAdmin.Base_de_datos
             return jsonString;
         }
 
+        /// <summary>
+        /// Carga un json a la clase
+        /// </summary>
         private void loadJson()
         {
             this.list = JsonConvert.DeserializeObject<List<T>>(jsonInfo);
             this.jsonInfo = this.getJsonString();
         }
 
+        /// <summary>
+        /// Guarda el json localmente
+        /// </summary>
         private void saveJson()
         {
             string tempJson = JsonConvert.SerializeObject(this.list);
@@ -48,6 +61,11 @@ namespace BaseDatosAdmin.Base_de_datos
             this.loadJson();
         }
 
+        /// <summary>
+        /// Añade un nuevo elemento generico tipo T a la lista
+        /// </summary>
+        /// <param name="newElement">Elemento genérico que se desea agregar</param>
+        /// <returns></returns>
         public string addElementToJson(T newElement)
         {
             this.list.Add(newElement);
